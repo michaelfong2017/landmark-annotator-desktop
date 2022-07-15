74791c153b046a730358355c13f035f5a4cdc18d
Michael Fong <michaelfong2017@gmail.com>
2022-07-15 21:23:41 +0800

    Rename Patient Tab. Time needs +8 hours and format rounds to seconds.

--- DesktopApp/patienttab.cpp
+++ DesktopApp/patienttab.cpp
@@ -133,6 +133,14 @@ void PatientTab::onFetchExistingImagesOfPatient(QNetworkReply* reply) {
                 QString text;
 
                 text = obj["aiDiagnosisDate"].toString();
+                QString dateStr = text.split("T")[0];
+                QString timeStr = text.split("T")[1].split(".")[0];
+
+                QDate date(dateStr.split("-")[0].toInt(), dateStr.split("-")[1].toInt(), dateStr.split("-")[2].toInt());
+                QTime time(timeStr.split(":")[0].toInt(), timeStr.split(":")[1].toInt(), timeStr.split(":")[2].toInt());
+                QDateTime localTime = QDateTime(date, time, Qt::UTC).toLocalTime();
+
+                text = localTime.toString("yyyy-MM-dd HH:mm:ss");
 
                 item = new QStandardItem(text);
                 QFont fn = item->font();
