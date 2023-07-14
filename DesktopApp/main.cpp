e55e4e0ebf04f39aeceb51252af6836753758566
Michael Fong <michaelfong2017@gmail.com>
2023-07-14 23:26:16 +0800

    Test OrbbecSDK

--- DesktopApp/main.cpp
+++ DesktopApp/main.cpp
@@ -9,6 +9,10 @@
 
 #include "cameramanager.h"
 
+#include "window.h"
+#include "libobsensor/hpp/Pipeline.hpp"
+#include "libobsensor/hpp/Error.hpp"
+
 void myMessageHandler(QtMsgType type, const QMessageLogContext&, const QString& msg)
 {
     //fprintf(stdout, "%s", msg.toLocal8Bit().constData());
@@ -47,6 +51,51 @@ void myMessageHandler(QtMsgType type, const QMessageLogContext&, const QString& 
     ts << txt << endl;
 }
 
+void orbbecTest() {
+    ob::Pipeline pipe;
+    std::shared_ptr<ob::Config> config = std::make_shared<ob::Config>();
+
+    std::shared_ptr<ob::VideoStreamProfile> colorProfile = nullptr;
+    try {
+        auto profiles = pipe.getStreamProfileList(OB_SENSOR_COLOR);
+        try {
+            colorProfile = profiles->getVideoStreamProfile(1280, 0, OB_FORMAT_RGB, 30);
+        }
+        catch (ob::Error& e) {
+            colorProfile = std::const_pointer_cast<ob::StreamProfile>(profiles->getProfile(0))->as<ob::VideoStreamProfile>();
+        }
+        config->enableStream(colorProfile);
+    }
+    catch (ob::Error& e) {
+        std::cerr << "Current device is not support color sensor!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    pipe.start(config);
+
+    Window app("ColorViewer", colorProfile->width(), colorProfile->height());
+
+    try {
+        if (pipe.getDevice()->isPropertySupported(OB_PROP_COLOR_MIRROR_BOOL, OB_PERMISSION_WRITE)) {
+            pipe.getDevice()->setBoolProperty(OB_PROP_COLOR_MIRROR_BOOL, true);
+        }
+    }
+    catch (const ob::Error& e) {
+        std::cerr << "Failed to set mirror property: " << e.getMessage() << std::endl;
+    }
+
+    while (app) {
+        auto frameSet = pipe.waitForFrames(100);
+        if (frameSet == nullptr) {
+            continue;
+        }
+
+        app.render({ frameSet->colorFrame() }, RENDER_SINGLE);
+    }
+
+    pipe.stop();
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -66,6 +115,8 @@ int main(int argc, char *argv[])
     //camera::CameraManager::getInstance().setCamera(camera::Model::KINECT);
     //qDebug() << endl << "width is " << camera::CameraManager::getInstance().getConfig()->color_width << endl;
     // Test implementing Strategy pattern and multiple inheritance END
+
+    orbbecTest();
 
     MainWindow w;
     w.show();
